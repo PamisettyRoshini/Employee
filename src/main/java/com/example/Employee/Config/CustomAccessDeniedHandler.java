021c9f83b55abc65249c527665e23eda09792202
pamisetty.roshini <roshinipamisett29@gmail.com>
2024-02-14 09:09:23 +0530

    corrected json parse error1

--- src/main/java/com/example/Employee/Config/CustomAccessDeniedHandler.java
+++ src/main/java/com/example/Employee/Config/CustomAccessDeniedHandler.java
@@ -2,11 +2,7 @@ package com.example.Employee.Config;
 
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
-import org.springframework.security.web.access.AccessDeniedHandlerImpl;
 import org.springframework.stereotype.Component;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
